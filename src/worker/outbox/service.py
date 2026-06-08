ec6deac54e19b7d70590d60c4be8e480b7de4d3a
Egor Kondratev <zhora_kondratev@bk.ru>
2026-06-08 08:35:18 +0300

    feat: except handler on outbox worker

--- src/worker/outbox/service.py
+++ src/worker/outbox/service.py
@@ -1,7 +1,11 @@
+import logging
+
 # Так как у нас распределенный монолит в примере, то используем инстанс консюмер брокера в качестве паблишера
 # (такой кейс можно рефакторить в /core, чтобы инстанс брокера не был физически привязан к домену консюмера)
 from src.core.brokers import BrokerRabbitMQ
 from src.worker.outbox.repository import OutboxWorkerRepository
+
+logger = logging.getLogger(__name__)
 
 
 class OutboxWorkerService:
@@ -15,15 +19,17 @@ class OutboxWorkerService:
         for event in events:
             payload = event.get("payload")
 
-            await self._publisher.publish(
-                message=payload,
-                message_id=payload["idempotency_key"],
-                queue="payments.new",
-                routing_key="payments.new",
-                exchange="payments",  # export to consts
-                persist=True,
-            )
-            await self._repository.mark_published(outbox_id=event.get("id"))
+            try:
+                await self._publisher.publish(
+                    message=payload,
+                    message_id=payload["idempotency_key"],
+                    queue="payments.new",  # todo: Очередь берем из БД
+                    exchange="payments",  # todo: Обменник берем из БД
+                    persist=True,
+                )
+                await self._repository.mark_published(outbox_id=event.get("id"))
+            except Exception:
+                logger.exception("Failed to publish event from outbox")
 
         # p.s. Так как текущая логики outbox умещается в 5 строк, то вся логика лежит внутри воркера,
         # но если он начнет разрастаться, то можно создать отдельный интерфейс под это всё дело ;)
